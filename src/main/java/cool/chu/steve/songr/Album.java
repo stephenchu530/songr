47f7c7540a3303072b838f50db3f18461d85faaf
STEPHEN CHU <stephen.chu530@gmail.com>
2019-06-14 08:36:01 -0700

    refactor: removed id setter and getter cuz useless

--- src/main/java/cool/chu/steve/songr/Album.java
+++ src/main/java/cool/chu/steve/songr/Album.java
@@ -26,14 +26,6 @@ public class Album {
         this.imageURL = imageURL;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
